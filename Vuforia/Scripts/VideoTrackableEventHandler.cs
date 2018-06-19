2039608136920b0251c60451c30912a8daddd851
kirilkhalil <37983740+kirilkhalil@users.noreply.github.com>
2018-06-20 01:07:05 +0300

    Required Vuforia files to replicate project.
    To replicate the whole project you will need to create a new Vuforia project in Unity and use the assets provided here to recreate the scene.

---
+++ Vuforia/Scripts/VideoTrackableEventHandler.cs
@@ -0,0 +1,15 @@
+﻿
+
+public class VideoTrackableEventHandler : DefaultTrackableEventHandler
+{
+   
+
+    protected override void OnTrackingLost()
+    {
+        //mTrackableBehaviour.GetComponentInChildren<Video_Skripti>().Pause();
+
+        base.OnTrackingLost();
+    }
+
+
+}
---
+++ Vuforia/Scripts/Video_Skripti.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class Video_Skripti : DefaultTrackableEventHandler{
+
+    private VideoPlayer videoPlayer;
+
+    //public Button playNappi;
+    // Use this for initialization
+    protected override void Start(){
+
+        base.Start();
+        videoPlayer = FindObjectOfType<VideoPlayer>().GetComponent<VideoPlayer>();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        if (videoPlayer) videoPlayer.Play();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        if (videoPlayer) videoPlayer.Pause();
+    }
+
+
+    // Update is called once per frame
+    /* void Update() {
+
+         if (videoPlayer.isPlaying)
+         {
+             ShowPlayButton(false);
+
+         }
+
+         else
+         {
+             ShowPlayButton(true);
+         }
+     }
+
+     void OnApplicationPause(bool pause)
+     {
+         if (pause)
+             Pause();
+     }
+
+     public void Play()
+     {      
+         //PauseAudio(false);
+         videoPlayer.Play();
+         ShowPlayButton(false);
+     }
+
+     public void Pause()
+     {
+         if (videoPlayer)
+         {
+             //PauseAudio(true);
+             videoPlayer.Pause();
+             ShowPlayButton(true);
+         }
+     }
+
+     private void ShowPlayButton(bool enable)
+     {
+         playNappi.enabled = enable;
+         playNappi.GetComponent<Image>().enabled = enable;
+     }*/
+}
